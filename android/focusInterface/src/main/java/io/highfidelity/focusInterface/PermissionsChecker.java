209d6116e81f0f70761ab17fa4fe010c7ef09719
amer cerkic <amer@highfidelity.io>
2019-02-28 14:10:01 -0800

    adding initial module for vive -focus

---
+++ android/focusInterface/src/main/java/io/highfidelity/focusInterface/PermissionsChecker.java
@@ -0,0 +1,12 @@
+package io.highfidelity.focusInterface;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PermissionsChecker extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
