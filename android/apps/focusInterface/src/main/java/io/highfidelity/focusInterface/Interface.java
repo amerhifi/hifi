f9617f3eb46726a1ceab21e4bee40357527ce0da
amer cerkic <amer@highfidelity.io>
2019-02-28 14:31:59 -0800

    adding interface activity and setting up the manifest

---
+++ android/apps/focusInterface/src/main/java/io/highfidelity/focusInterface/Interface.java
@@ -0,0 +1,12 @@
+package io.highfidelity.focusInterface;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Interface extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
